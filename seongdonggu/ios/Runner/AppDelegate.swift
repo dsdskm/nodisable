c0b2a09f1ae9d5a727d3acdd1760178d58f50c07
dsdskm <tothetg@naver.com>
2020-12-17 01:51:04 +0900

    init for ios

--- seongdonggu/ios/Runner/AppDelegate.swift
+++ seongdonggu/ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import Firebase
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +9,10 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()
+    GMSServices.provideAPIKey("AIzaSyABmYv4KCOQfOSt3b6ViE2bvtZ_CYBx9mA")
     GeneratedPluginRegistrant.register(with: self)
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
