4130c7e5a175bebd616804d5117b78302c3dc871
dsdskm <tothetg@naver.com>
2020-12-20 15:05:26 +0900

    Android

--- seongdonggu/lib/common/cahced.dart
+++ seongdonggu/lib/common/cahced.dart
@@ -50,22 +50,41 @@ List<Position> FAKE_POST_LIST = [
   Position(latitude: 37.548469, longitude: 126.823195),
 ];
 List<Position> FAKE_POST_LIST2 = [
-  Position(latitude: 37.547988, longitude: 126.822802),
-  Position(latitude: 37.548069, longitude: 126.822838),
-  Position(latitude: 37.548081, longitude: 126.822768),
-  Position(latitude: 37.548060, longitude: 126.822671),
-  Position(latitude: 37.548040, longitude: 126.822628),
-  Position(latitude: 37.548038, longitude: 126.822574),
+  getPos(37.548022, 126.822496),
+  getPos(37.54805601129031, 126.82252464232351),
+  getPos(37.54805246188759, 126.82256717198689),
+  getPos(37.548048912484695, 126.82262760887696),
+  getPos(37.548073758301406, 126.82264999291029),
+  getPos(37.548061335394095, 126.8226835689603),
+  getPos(37.54806311009526, 126.82271042980034),
+  getPos(37.54806843419854, 126.8227260986237),
+  getPos(37.54807198360049, 126.82275519786705),
+  getPos(37.54807198360049, 126.82277982030375),
+  getPos(37.548079082403916, 126.82280891954711),
+  getPos(37.54808440650604, 126.82284249559716),
+  getPos(37.54809505470913, 126.82289397887386),
+  getPos(37.54809860410984, 126.82295441576392),
+  getPos(37.54810037881012, 126.8229835150073),
+  getPos(37.54808263180536, 126.823034998284),
+  getPos(37.54805956069288, 126.82310662719075),
+  getPos(37.5480648847964, 126.82315363366078),
+  getPos(37.54804358838005, 126.8232051169375),
+  getPos(37.548022291957615, 126.82327226903756),
+  getPos(37.54802406665972, 126.82331479870095),
+  getPos(37.54801519314877, 126.82336180517098),
+  getPos(37.54800277023169, 126.8234311956744),
 ];
+
+getPos(double d, double e) {
+  return Position(latitude: d, longitude: e);
+}
 
 int fake_index = 0;
 
 getFakePosition() {
-
-  if (fake_index < FAKE_POST_LIST.length - 1) {
+  if (fake_index < FAKE_POST_LIST2.length - 1) {
     fake_index++;
   }
-  print("getFakePosition ${FAKE_POST_LIST[fake_index]}");
-  return FAKE_POST_LIST[fake_index];
+  print("getFakePosition ${FAKE_POST_LIST2[fake_index]}");
+  return FAKE_POST_LIST2[fake_index];
 }
-
